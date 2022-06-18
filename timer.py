d2504940d25c37eaf7f5e9b4a425d3402c4b8437
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-06-18 11:26:46 -0400

    Added sound_alarm()

--- timer.py
+++ timer.py
@@ -2,6 +2,13 @@ import argparse
 import time
 
 
+def sound_alarm(rings=12, freq=5):
+    print('Control-C to stop alarm.')
+    for i in range(rings):
+        print('\a', end='\r')
+        time.sleep(freq)
+
+
 def pomodoro_timer_cli(minutes):
     while minutes:
         print(f'{minutes} minute{"s" if minutes > 1 else ""} to go', end="\r")
@@ -15,5 +22,6 @@ parser.add_argument("minutes", help='number of minutes to set timer (1-120)',
 args = parser.parse_args()
 if 1 <= args.minutes <= 120:
     pomodoro_timer_cli(args.minutes)
+    sound_alarm()
 else:
     print('The timer must be set to an integer value between 1 and 120.')
