c53df07fee6ded6d49cacc2e1c64e4b9f4be6f24
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-06-18 12:29:50 -0400

    Moved keyboard exception processing to main

--- timer.py
+++ timer.py
@@ -4,27 +4,30 @@ import time
 
 def sound_alarm(rings=12, freq=5):
     print('Control-C to stop alarm.')
-    try:
-        for i in range(rings):
-            print('\a', end='\r')
-            time.sleep(freq)
-    except KeyboardInterrupt:
-        return
+    for i in range(rings):
+        print('\a', end='\r')
+        time.sleep(freq)
 
 
 def pomodoro_timer_cli(minutes):
     while minutes:
-        print(f'{minutes} minute{"s" if minutes > 1 else ""} to go', end="\r")
+        print(f'{minutes} minute{"s" if minutes > 1 else ""} to go...',
+              end='\r')
         time.sleep(60)
         minutes -= 1
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("minutes", help='number of minutes to set timer (1-120)',
-                    type=int)
-args = parser.parse_args()
-if 1 <= args.minutes <= 120:
-    pomodoro_timer_cli(args.minutes)
-    sound_alarm()
-else:
-    print('The timer must be set to an integer value between 1 and 120.')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("minutes",
+                        help='number of minutes to set timer (1-120)',
+                        type=int)
+    args = parser.parse_args()
+    if 1 <= args.minutes <= 120:
+        try:
+            pomodoro_timer_cli(args.minutes)
+            sound_alarm()
+        except KeyboardInterrupt:
+            exit()
+    else:
+        print('The timer must be set to an integer value between 1 and 120.')
