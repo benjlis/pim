5eeb7cc0a5c31cf26e4626f847feca78fd20c532
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-06-26 07:47:45 -0400

    Updated to new schema

--- dml.sql
+++ dml.sql
@@ -1,34 +1,32 @@
 -- name: get_life_areas
 -- Get all the greetings in the database
-select area_id, code, description from life_areas;
+select area, code, description from life_areas;
 
 -- name: add_project!
-insert into projects(area_id, code, description)
-select area_id, :code, :description
-   from life_areas where code = :area_code;
+insert into projects(project, area, description)
+    values (:project, :area, :description);
 
 -- name: list_projects
-select a.code, p.project_id, p.code, p.description
-    from projects p join life_areas a on (p.area_id = a.area_id);
+select a.area, p.project_id, p.project, p.description
+    from projects p join life_areas a on (p.area = a.area);
 
 -- name: add_activity!
-insert into activities(code, description) values (:code, :description);
+insert into activities(activity, area, description)
+    values (:activity, :area, :description);
 
 -- name: list_activities
 select * from activities;
 
--- name: done!
-insert into tasks (area_id, activity_id, duration, description, completed,
+-- name: task!
+insert into tasks (activity_id, duration, description, completed,
                    project_id)
-values ((select area_id from life_areas where code = :area_code),
-        (select activity_id from activities where code = :activity_code),
+values ((select activity_id from activities where activity = :activity),
         :duration, :description, current_date,
-        (select project_id from projects where code = :project_code));
+        (select project_id from projects where project = :project));
 
 -- name: done_today
-select l.code, p.code, a.code, t.duration, t.description, t.created
-    from tasks t join life_areas l on (t.area_id = l.area_id)
-                 join activities a on (t.activity_id = a.activity_id)
+select a.activity, p.project, t.duration, t.description, t.created
+    from tasks t join activities a on (t.activity_id = a.activity_id)
                  left join projects p on (t.project_id = p.project_id)
     where t.completed = current_date
     order by t.completed;
